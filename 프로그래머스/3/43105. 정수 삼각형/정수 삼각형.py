25a31f49c145bb83130229c9aa166ba90f823ed3
임태규 <127077818+dlawork9888@users.noreply.github.com>
2024-04-13 22:01:19 +0900

    [level 3] Title: 정수 삼각형, Time: 34.11 ms, Memory: 14.4 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,26 +1,6 @@
-# 또 풀기
-
 def solution(triangle):
-    # Top - Down
-    # 따로 메모이제이션 리스트를 두지 말자
-    # 바로 경로값을 갱신하자 !
-    for floor, vals in enumerate(triangle):
-        # 0층은 아무것도 하지 않는다
-        if floor == 0:
-            continue
-        # 층 접근
-        for idx, val in enumerate(vals):
-            # 맨 앞
-            if idx == 0:
-                vals[idx] += triangle[floor - 1][0]
-            # 맨 뒤
-            elif idx == floor:
-                vals[idx] += triangle[floor - 1][floor - 1]
-            # 가운데
-            else:
-                if triangle[floor - 1][idx - 1] < triangle[floor - 1][idx]:
-                    vals[idx] += triangle[floor - 1][idx]
-                else:
-                    vals[idx] += triangle[floor - 1][idx - 1]
-    triangle[-1].sort()
-    return triangle[-1][-1]
+    for floor in range(len(triangle) - 2, -1, -1):
+        for idx, ele in enumerate(triangle[floor]):
+            triangle[floor][idx] += triangle[floor + 1][idx] if triangle[floor + 1][idx] > triangle[floor + 1][idx + 1] else triangle[floor + 1][idx + 1]
+    return triangle[0][0]
+            
