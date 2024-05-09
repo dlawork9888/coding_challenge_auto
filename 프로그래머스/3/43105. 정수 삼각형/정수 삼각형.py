23be88b4acd0ec5aec75386009ef9a88a11df5e3
임태규 <127077818+dlawork9888@users.noreply.github.com>
2024-05-09 14:27:45 +0900

    [level 3] Title: 정수 삼각형, Time: 33.26 ms, Memory: 14.4 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,6 +1,9 @@
+# Bottom - Up
+# Golf !
+
 def solution(triangle):
     for floor in range(len(triangle) - 2, -1, -1):
-        for idx, ele in enumerate(triangle[floor]):
+        for idx in range(len(triangle[floor])):
             triangle[floor][idx] += triangle[floor + 1][idx] if triangle[floor + 1][idx] > triangle[floor + 1][idx + 1] else triangle[floor + 1][idx + 1]
     return triangle[0][0]
-            
+    
