25288201a1660cf868af918666eab0b895dc0752
임태규 <127077818+dlawork9888@users.noreply.github.com>
2024-05-16 19:26:29 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,30 +1,16 @@
-# 장르 별 Top2
-# 장르 > 재생 수 (재생 수 같다면 고유번호 낮은 기준)
-
-from collections import defaultdict as dd
-
-#def for_sort(x):
-    
-
+from collections import defaultdict
 def solution(genres, plays):
-    
-    # 1.
-    # 장르 재생수 defaultdict
-    # 장르별 재생 수와 고유번호
-    # 이 둘의 한 번의 순회로
-    
-    play_of_genre = dd(int)
-    play_and_idx = dd(list)
-    
-    for idx, genre in enumerate(genres):
-        play_of_genre[genre] += plays[idx]
-        play_and_idx[genre].append((plays[idx], idx)) #(재생 수, 고유번호)
-    
-    answer = []
-    for item in sorted(play_of_genre.items(), key = lambda x: -x[1]):
-        genre = item[0]
-        answer += [x[1] for x in sorted( play_and_idx[genre], key = lambda x: (-x[0],x[1]) )[:2]]
-
-    return answer
-    
-    
+    genres_play = defaultdict(int)  # 장르: 총 재생수
+    genres_idx = defaultdict(list)  # 장르:[(재생수, 고유번호),(재생수, 고유번호), ...]
+    for idx, (genre, play) in enumerate(zip(genres, plays)):
+        # 장르별 총 재생 수 계산
+        genres_play[genre] += play
+        # 장르별 (재생수, 고유번호) 분류
+        genres_idx[genre].append((play, idx))
+    result = []
+    # 총재생수의 내림차순으로 정리된 genres_play.items()
+    for genre, _ in sorted(genres_play.items(), key=lambda genre_play: -genre_play[1]):  # (장르, 재생수)에서 재생수의 내림차순으로 정렬
+        # (재생수, 고유번호)에서 재생수는 내림차순, 재생수 같다면 고유번호 오름차순 => [:2] => 고유번호만
+        result.extend(
+            [ele[1] for ele in sorted(genres_idx[genre], key=lambda play_idx: (-play_idx[0], play_idx[1]))[:2]])
+    return result
