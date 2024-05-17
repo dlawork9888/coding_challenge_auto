856cb82633321cf0ae8ce494eabcf39a96c2d476
임태규 <127077818+dlawork9888@users.noreply.github.com>
2024-05-17 11:28:24 +0900

    Update 전력망을 둘로 나누기.py

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,51 +1,46 @@
-from itertools import combinations
-from collections import deque
-import copy
-import math 
+# Disjoint Set도 괜찮을듯
+# 간선 하나 없애고 그걸로 인접리스트 만들기
+# => 해당 그래프를 탐색하며 노드 새기
+# => 모든 노드를 탐색할 때까지
+from collections import defaultdict, deque
 
-def return_adj_mat(n,wires):
-    adj_mat = [[False for x in range(n)] for x in range(n)]
+# 간선 하나 없어진 wires가 인자로
+def make_adj_list_and_search(n, wires):
+    # 인접 리스트 생성
+    adj_list = defaultdict(list)
+    # 노드는 1번부터 시작임 => -1을 해주자
+    for node1, node2 in wires:
+        adj_list[node1 - 1].append(node2 - 1)
+        adj_list[node2 - 1].append(node1 - 1)
     
-    for wire in wires:
-        adj_mat[wire[0]-1][wire[1]-1] = True
-        adj_mat[wire[1]-1][wire[0]-1] = True
+    visited = [False for _ in range(n)]
+    counts = []
+    while all(visited) == False:
+        count = 0
+        start_node = -1
+        for idx, visited_value in enumerate(visited):
+            if visited_value == False: 
+                start_node = idx
+                break
+        q = deque()
+        q.append(start_node)
+        visited[start_node] = True
+        while q:
+            now_node = q.pop() #DFS
+            count += 1
+            # 주위 노드 탐색
+            for node in adj_list[now_node]:
+                if visited[node] == False:
+                    q.append(node)
+                    visited[node] = True
+        counts.append(count)
+    #print(f'counts:{counts}')
+    return abs(counts[0] - counts[1])
     
-    return adj_mat
-
-
-def return_after(n, input_adj_mat):
-    adj_mat = copy.deepcopy(input_adj_mat)
-    queue = deque()
-    
-    #시작 노드
-    start_node = -1
-    for i in range(n):
-        for j in range(n):
-            if adj_mat[i][j] == True:
-                start_node = i
-
-    queue.append(start_node)
-    count = 1
-    while queue:
-        now_node = queue.pop()
-        for i in range(n):
-            if adj_mat[now_node][i] == True:
-                queue.append(i)
-                count += 1
-                adj_mat[now_node][i] = False
-                adj_mat[i][now_node] = False
-    
-    return count, adj_mat
-
-
 def solution(n, wires):
-    coms = combinations(wires, n-2)
-    min_count = math.inf 
-    for com in coms:
-        com = list(com)
-        input_adj_mat = return_adj_mat(n, com)
-        count_1, input_adj_mat = return_after(n, input_adj_mat)
-        count_2, input_adj_mat = return_after(n, input_adj_mat)
-        if min_count > abs(count_1 - count_2):
-            min_count = abs(count_1 - count_2)
-    return min_count
+    diffs = []
+    for idx, wire in enumerate(wires):
+        diff = make_adj_list_and_search(n, wires[0:idx] + wires[idx + 1:])
+        diffs.append(diff)
+    return min(diffs)
+    
