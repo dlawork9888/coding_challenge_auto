3d07f5c30df0bec515c791a31c3473060f69b85a
임태규 <127077818+dlawork9888@users.noreply.github.com>
2024-05-17 10:19:44 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.51 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 8.11 ms
+메모리: 10.4 MB, 시간: 6.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 08일 16:17:14
+2024년 05월 17일 10:19:42
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,33 +1,16 @@
-from collections import deque as dq
-
-def solution(maps):
-    # 경로의 최솟값 => BFS
-    # visited는 방문여부만이 아니라, 해당 노드까지의 경로 길이를 기록해야 한다.
-    
-    # directions
-    d_row = [-1, 0, 1, 0]
-    d_col = [0, -1, 0, 1]
-    # visited 
-    visited = [[-1 for _ in range(len(maps[0]))] for _ in range(len(maps))]
-    # BFS 
-    q = dq()
-    start_node = [0,0] # 시작 좌표 => [row, col]
-    visited[start_node[0]][start_node[0]] = 1 # 방문 표시, 경로값은 1
-    q.append(start_node)
+from collections import deque
+def solution(game_map):
+    d_row = [0,1,0,-1]
+    d_col = [1,0,-1,0]
+    q = deque()
+    q.append((0,0))
     while q:
-        now_node = q.popleft()
-        # 다음 노드 탐색
+        now_row, now_col = q.popleft()
         for i in range(4):
-            next_row = now_node[0] + d_row[i]
-            next_col = now_node[1] + d_col[i]
-            # 1. 좌표가 맵 안에 있나?
-            # 2. 갈 수 있는 노드인가? (벽이 아닌가?) => 갈 수 있다면, 미방문인가?
-            if 0 <= next_row < len(maps) and 0 <= next_col < len(maps[0]): 
-                if maps[next_row][next_col] != 0 and visited[next_row][next_col] == -1:
-                    # 그러면 방문 표시(현재 노드의 경로값 + 1)하고 큐에 넣자
-                    visited[next_row][next_col] = visited[now_node[0]][now_node[1]] + 1
-                    q.append([next_row, next_col])
-    return visited[-1][-1]
-        
-        
-    
+            next_row = now_row + d_row[i]
+            next_col = now_col + d_col[i]
+            if 0 <= next_row < len(game_map) and 0 <= next_col < len(game_map[0]):
+                if game_map[next_row][next_col] == 1:
+                    q.append((next_row, next_col))
+                    game_map[next_row][next_col] = game_map[now_row][now_col] + 1
+    return -1 if game_map[-1][-1] == 1 or game_map[-1][-1] == 0 else game_map[-1][-1]
