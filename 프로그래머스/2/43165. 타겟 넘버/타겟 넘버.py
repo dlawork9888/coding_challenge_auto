aaa05b778298c10e30d8dee50d908b7f5428ef61
임태규 <127077818+dlawork9888@users.noreply.github.com>
2024-10-11 13:41:30 +0900

    [level 2] Title: 타겟 넘버, Time: 560.16 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,26 +1,14 @@
-# recursive DFS를 써보자 !
-
 def solution(numbers, target):
-    count = [0]
-
-    start_node = [0, 0] # 다음에 더하거나 뺄 원소의 인덱스, calculation result
-
-    def dfs_recursive(node):
-        if node[0] == len(numbers):
-            if node[1] == target:
-                count[0] += 1
-        else: # 다음 재귀
-            
-            now_index = node[0]
-            next_index = node[0] + 1
-            result = node[1]
-            
-            dfs_recursive([next_index, result + numbers[now_index]])
-            dfs_recursive([next_index, result - numbers[now_index]])
-            
-        return
-    
-    dfs_recursive(start_node)
-    
-    return count[0]
-    
+    count = 0
+    stack = []
+    stack.append([numbers[0],0])
+    stack.append([-numbers[0],0]) # 계산결과, 인덱스
+    while stack:
+        result, idx = stack.pop()
+        if idx < len(numbers) - 1:
+            stack.append([result - numbers[idx + 1], idx + 1])
+            stack.append([result + numbers[idx + 1], idx + 1])
+        else:
+            if result == target:
+                count += 1
+    return count
