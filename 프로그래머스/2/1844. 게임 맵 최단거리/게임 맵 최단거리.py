8848a52d59cfd727aa0dc8c81da1d0eae8a4fede
임태규 <127077818+dlawork9888@users.noreply.github.com>
2024-10-11 13:58:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 15.54 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,18 +1,19 @@
-from collections import deque
-def solution(game_map):
+from collections import deque as dq
+
+def solution(maps):
     d_row = [0,1,0,-1]
     d_col = [1,0,-1,0]
-    q = deque()
-    q.append((0,0))
+    # BFS
+    q = dq()
+    start_node = [0,0]
+    q.append(start_node)
     while q:
         now_row, now_col = q.popleft()
         for i in range(4):
             next_row = now_row + d_row[i]
             next_col = now_col + d_col[i]
-            if 0 <= next_row < len(game_map) and 0 <= next_col < len(game_map[0]):
-                if game_map[next_row][next_col] == 1:
-                    q.append((next_row, next_col))
-                    game_map[next_row][next_col] = game_map[now_row][now_col] + 1
-    return -1 if game_map[-1][-1] == 1 or game_map[-1][-1] == 0 else game_map[-1][-1]
-
-#visited 배열 없이 푸는 법
+            # in maps ? is able to go ?
+            if 0 <= next_row < len(maps) and 0 <= next_col < len(maps[0]) and maps[next_row][next_col] == 1:
+                maps[next_row][next_col] = maps[now_row][now_col] + 1 
+                q.append([next_row, next_col])
+    return -1 if maps[-1][-1] == 1 else maps[-1][-1]
